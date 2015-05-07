9e1b1f12afa224a10adb1a48eb54b07ebae9a33d
albort <alb.ort@hotmail.com>
2015-05-07 11:41:37 -0500

    Clean Instalation
    Clean installation of larvel APP

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,99 @@
+<?php namespace App\Http\Controllers\Auth;
+
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Contracts\Auth\Registrar;
+use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+
+
+class AuthController extends Controller {
+
+	/*
+	|--------------------------------------------------------------------------
+	| Registration & Login Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller handles the registration of new users, as well as the
+	| authentication of existing users. By default, this controller uses
+	| a simple trait to add these behaviors. Why don't you explore it?
+	|
+	*/
+
+	use AuthenticatesAndRegistersUsers;
+
+	public function postLogin(Request $request)
+	{
+		$this->validate($request, [
+			'email' => 'required|email', 'password' => 'required',
+		]);
+
+		$credentials = $request->only('email', 'password');
+
+		if ($this->auth->attempt($credentials)) {
+			$user = \Auth::user();
+			return response()->json([
+					'message' => 'Bienvenido '.$user['name'].'.',
+					'data' => [
+						'id' => $user['id'],
+						'name' => $user['name'],
+						'email' => $user['email'],
+						'address' => $user['address']
+					], 'code' => 200
+				], 200);
+		} else {
+			return response()->json([
+					'message' => 'El usuario o contrasena son incorrectos', 'code' => 200
+				], 404);
+		}
+	}
+
+	/**
+	 * Handle a registration request for the application.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function postRegister(Request $request)
+	{
+		$validator = $this->registrar->validator($request->all());
+
+		if ($validator->fails())
+		{
+			$this->throwValidationException(
+				$request, $validator
+			);
+		}
+
+		$this->auth->login($this->registrar->create($request->all()));
+
+		return redirect($this->redirectPath());
+	}
+/**
+	 * Log the user out of the application.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function getLogout()
+	{
+		$this->auth->logout();
+
+		return 'LOGOUT';
+	}
+	/**
+	 * Create a new authentication controller instance.
+	 *
+	 * @param  \Illuminate\Contracts\Auth\Guard  $auth
+	 * @param  \Illuminate\Contracts\Auth\Registrar  $registrar
+	 * @return void
+	 */
+	public function __construct(Guard $auth, Registrar $registrar)
+	{
+		$this->auth = $auth;
+		$this->registrar = $registrar;
+
+		$this->middleware('guest', ['except' => 'getLogout']);
+	}
+
+}
