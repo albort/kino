9daf4529632b4678c3a5c0e38bf545da29248ec2
albort <alb.ort@hotmail.com>
2015-05-13 11:38:40 -0500

    Updated database configuration

--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
 	|
 	*/
 
-	'default' => 'mysql',
+	'default' => 'pgsql',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -66,10 +66,10 @@ return [
 
 		'pgsql' => [
 			'driver'   => 'pgsql',
-			'host'     => env('DB_HOST', 'localhost'),
-			'database' => env('DB_DATABASE', 'forge'),
-			'username' => env('DB_USERNAME', 'forge'),
-			'password' => env('DB_PASSWORD', ''),
+			'host'     => env('DB_HOST', 'ec2-23-21-76-246.compute-1.amazonaws.com'),
+			'database' => env('DB_DATABASE', 'd6dp4l5l75sr21'),
+			'username' => env('DB_USERNAME', 'bnifqgipghlbuh'),
+			'password' => env('DB_PASSWORD', 'ScgKfyKQfRDuPUxu5Pg3_ojXp3'),
 			'charset'  => 'utf8',
 			'prefix'   => '',
 			'schema'   => 'public',
